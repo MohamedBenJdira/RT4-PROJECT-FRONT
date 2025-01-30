f15c235f4bae94f921afcb3242c2426c9d74e91b
Iheb Alimi <iheb.alimi@insat.ucar.tn>
2025-01-30 10:30:42 +0100

    feat: Add Not Found (404) page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { NotFoundPageComponent } from "./not-found/not-found-page.component";
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: LandingComponent },
+  { path: "not-found", component: NotFoundPageComponent },
+  { path: "**", redirectTo: "not-found" }
 ];
 
 @NgModule({
