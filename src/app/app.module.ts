c18060c77060b85afe4232d7fb77141bd87ddd60
Hama02 <mohamed.ben.jdira@mindbay.ai>
2025-02-01 17:33:29 +0100

    feat: implementing auth flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { RegistrationPageComponent } from './pages/auth/registration-page.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ErrorMessageComponent } from './pages/auth/error-message/error-message.component';
 
 @NgModule({
   declarations: [
@@ -13,8 +18,17 @@ import { LandingComponent } from './pages/landing/landing.component';
     NavbarComponent,
     FooterComponent,
     LandingComponent,
+    RegistrationPageComponent,
+    ErrorMessageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
